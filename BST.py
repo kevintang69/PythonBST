48ed4d00f0745006831fd62f5b82a0c73066c1c4
Kevin Tang <kevin@example.com>
2022-09-11 16:20:25 -0700

    done BST

--- BST.py
+++ BST.py
@@ -2,29 +2,48 @@ from turtle import left, right
 
 
 class BST:
-    NODE_WIDTH = 2
     def __init__(self, key):
         self.key = key
         self.right = None
         self.left = None
-        self.width = BST.NODE_WIDTH
+        self.width = len(str(key))
         self.height = 0
+        self.l = LNode(key)
     def insert(self, key):
         
         if key < self.key:
             if self.left is None:
                 self.left = BST(key)
+                successor = self.l
+                new_node = self.left.l
+                predecessor = successor.prev
+                if predecessor is not None:
+                    predecessor.next = new_node
+                new_node.prev = predecessor
+                new_node.next = successor
+                successor.prev = new_node
             else:
                 self.left = self.left.insert(key)
         elif key > self.key:
             if self.right is None:
                 self.right = BST(key)
+                predecessor = self.l
+                new_node = self.right.l
+                successor = predecessor.next
+                if successor is not None:
+                    successor.prev = new_node
+                new_node.prev = predecessor
+                new_node.next = successor
+                predecessor.next = new_node
             else:
                 self.right = self.right.insert(key)
                 
         self.update_height()
         
         root = self.rebalance()
+        root.update_width()
+        self.update_width()
+        
         return root
         
     
@@ -39,7 +58,7 @@ class BST:
         self.height = 1 + max(right_height, left_height)
 
     def update_width(self):
-        total = BST.NODE_WIDTH
+        total = len(str(self.key))
         if self.left is not None:
             total += self.left.width
         if self.right is not None:
@@ -61,28 +80,32 @@ class BST:
                     current_line += (left_child_pos - len(current_line)) * " "
                     current_line += (pos - len(current_line)) * "_"
                 current_line += (pos - len(current_line))*" "
-                current_line += node.zeropad()
+                current_line += str(node.key)
                 if node.right is not None:
-                    right_start = pos+BST.NODE_WIDTH
+                    right_start = pos+len(str(node.key))
                     right_child_pos = node.right.compute_pos(right_start)
                     newq.append((right_start,right_child_pos,node.right))
-                    current_line += (right_child_pos - len(current_line) + BST.NODE_WIDTH) * "_"
+                    current_line += (right_child_pos - len(current_line) + len(str(node.right.key)) ) * "_"
             q = newq
             total_str += current_line + "\n"
         return total_str
 
+    def linked_list(self):
+        smallest = self
+        while smallest.left is not None:
+            smallest = smallest.left
+        head = smallest.l
+        ordered = []
+        while head is not None:
+            ordered.append(head.key)
+            head = head.next
+        return ordered
 
     def compute_pos(self,start):
         pos = start
         if self.left is not None:
             pos += self.left.width
         return pos
-
-
-    def zeropad(self):
-        if self.key < 10:
-            return "0" + str(self.key)
-        return str(self.key)
 
     def rebalance(self):
         right_height = -1
@@ -116,12 +139,6 @@ class BST:
             if right_of_right_height < left_of_right_height:
                 self.right = self.right.right_rotate()
             root = self.left_rotate()
-
-
-        
-        self.update_width()
-        # root.update_width()
-        
         return root
 
 
@@ -141,27 +158,65 @@ class BST:
         self.update_height()
         root.update_height()
         return root
+    
+    def __iter__(self):
+        smallest = self
+        while smallest.left is not None:
+            smallest = smallest.left
+        head = smallest.l
+        return head.__iter__()
 
+
+        
+
+class LNode:
+    def __init__(self,key):
+        self.key = key
+        self.next = None
+        self.prev = None
+    def __iter__(self):
+        current = self
+        while current is not None:
+            yield current.key
+            current = current.next
+
+class Tree:
+    def __init__(self):
+        self.t = None
+
+    def insert(self, key):
+        if self.t is None:
+            self.t = BST(key)
+        else:
+            self.t = self.t.insert(key)
+    def print_tree(self):
+        return self.t.draw()
+    def print_list(self):
+        return self.t.linked_list()
+    def __iter__(self):
+        return self.t.__iter__()
 
                 
 
-tree = BST(5)
-tree = tree.insert(1)
-tree = tree.insert(3)
-tree = tree.insert(2)
-tree = tree.insert(4)
-tree = tree.insert(13)
-tree = tree.insert(19)
-tree = tree.insert(20)
-tree = tree.insert(10)
-tree = tree.insert(8)
-tree = tree.insert(12)
+tree = Tree()
+tree.insert(5)
+tree.insert(1)
+tree.insert(3)
+tree.insert(2)
+tree.insert(4)
+tree.insert(13)
+tree.insert(19)
+tree.insert(20)
+tree.insert(10)
+tree.insert(8)
+tree.insert(12)
+tree.insert(9)
+tree.insert(21)
 
+tree.insert(22)
 
+tree.insert(23)
+tree.insert(24)
+tree.insert(25)
 
-
-
-print(tree.draw())
-
-
-   
+print(tree.print_tree())
