88a574618a640ea6f80850ed993c921f8725911e
Kevin Tang <kevin@example.com>
2022-09-13 21:55:11 -0700

    removed print statements

--- BST.py
+++ BST.py
@@ -198,25 +198,34 @@ class Tree:
 
                 
 
-tree = Tree()
-tree.insert(5)
-tree.insert(1)
-tree.insert(3)
-tree.insert(2)
-tree.insert(4)
-tree.insert(13)
-tree.insert(19)
-tree.insert(20)
-tree.insert(10)
-tree.insert(8)
-tree.insert(12)
-tree.insert(9)
-tree.insert(21)
+# tree = Tree()
+# tree.insert(5)
+# tree.insert(1)
+# tree.insert(3)
+# tree.insert(2)
+# tree.insert(4)
+# tree.insert(13)
+# tree.insert(19)
+# tree.insert(20)
+# tree.insert(10)
+# tree.insert(8)
+# tree.insert(12)
+# tree.insert(9)
+# tree.insert(21)
 
-tree.insert(22)
+# tree.insert(22)
 
-tree.insert(23)
-tree.insert(24)
-tree.insert(25)
+# tree.insert(23)
+# tree.insert(24)
+# tree.insert(25)
 
-print(tree.print_tree())
+# print(tree.print_tree())
+
+
+
+def func():
+    return 3
+    if False:
+        yield 3
+print([item for item in func()])
+print(func())
